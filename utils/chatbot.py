db681735433a4ca65b0faceb4a28ba1c5a551fd3
shreyagowda-19 <sj19052005@gmail.com>
2026-07-21 01:23:07 +0530

    Update Gemini model

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash-lite",
+    model="gemini-3.5-flash",
     google_api_key=st.secrets["GOOGLE_API_KEY"],
     temperature=0
 )
