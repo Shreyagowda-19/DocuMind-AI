bb2cb670052d3ebbbfb5e0a8dfb88da913e61187
shreyagowda-19 <sj19052005@gmail.com>
2026-07-21 00:53:51 +0530

    Use Streamlit secrets for Gemini API

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -1,28 +1,21 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
+    google_api_key=st.secrets["GOOGLE_API_KEY"],
     temperature=0
 )
 
 def ask_question(retriever, question):
 
-    # Retrieve relevant chunks
     if "summary" in question.lower() or "summarize" in question.lower():
         docs = retriever.invoke("main topics of the document")
     else:
         docs = retriever.invoke(question)
 
-    # Combine retrieved chunks
-    context = "\n\n".join(
-        [doc.page_content for doc in docs]
-    )
+    context = "\n\n".join([doc.page_content for doc in docs])
 
-    # Prompt
     prompt = f"""
 You are an intelligent Research Assistant.
 
